0527b7f327f8163b70452fb1d25a4a921dd6d580
Elias Tracy <elitracy19@gmail.com>
2026-01-22 00:01:57 -0600

    feat: added info table
    - added info table pane
    - updated planet info pane to use info table pane

---
+++ ui/infoTable.go
@@ -0,0 +1,71 @@
+package ui
+
+import (
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type InfoTablePane struct {
+	*Pane
+	table   table.Model
+	keymaps map[string]func() tea.Cmd
+	onEsc   func() tea.Cmd
+	theme   UITheme
+}
+
+func NewInfoTablePane(table table.Model, keymaps map[string]func() tea.Cmd) *InfoTablePane {
+	pane := &InfoTablePane{
+		Pane:    &Pane{},
+		table:   table,
+		keymaps: keymaps,
+	}
+	return pane
+}
+
+func (p *InfoTablePane) Init() tea.Cmd {
+	return nil
+}
+
+func (p *InfoTablePane) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+
+		key := msg.String()
+		if handler, ok := p.keymaps[key]; ok {
+			return p, handler()
+		}
+
+		switch msg.String() {
+		case "k", "up":
+			p.table.MoveUp(1)
+		case "j", "down":
+			p.table.MoveDown(1)
+		case "ctrl+c", "q":
+			return p, tea.Quit
+		}
+	}
+
+	return p, nil
+}
+
+func (p *InfoTablePane) View() string {
+	p.theme = GetPaneTheme(p)
+
+	tableStyles := table.DefaultStyles()
+
+	tableStyles.Header = tableStyles.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(p.theme.DimmedStyle.GetForeground()).
+		BorderBottom(true).
+		Bold(false)
+
+	tableStyles.Selected = tableStyles.Selected.
+		Foreground(p.theme.FocusedStyle.GetBackground()).
+		Background(p.theme.FocusedStyle.GetForeground()).
+		Bold(false)
+
+	p.table.SetStyles(tableStyles)
+
+	return p.table.View()
+}
--- ui/planetInfo.go
+++ ui/planetInfo.go
@@ -2,7 +2,9 @@ package ui
 
 import (
 	"fmt"
+	"strconv"
 
+	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/elitracy/planets/core"
@@ -14,50 +16,57 @@ import (
 type PlanetInfoPane struct {
 	*Pane
 
-	childPaneID core.PaneID
-	planet      *models.Planet
-	theme       UITheme
+	infoTable ManagedPane
+	planet    *models.Planet
+	theme     UITheme
 }
 
-func (p *PlanetInfoPane) Init() tea.Cmd { return nil }
+func (p *PlanetInfoPane) Init() tea.Cmd {
+	keymaps := make(map[string]func() tea.Cmd)
+
+	keymaps["esc"] = func() tea.Cmd {
+		return tea.Sequence(popDetailStackCmd(), popFocusStackCmd())
+	}
+
+	infoTable := p.createInfoTable()
+	p.infoTable = NewInfoTablePane(
+		infoTable,
+		keymaps,
+	)
+	PaneManager.AddPane(p.infoTable)
+
+	p.infoTable.Init()
+
+	return nil
+}
 func (p *PlanetInfoPane) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
-	var paneID core.PaneID
-
+	var cmds []tea.Cmd
 	switch msg := msg.(type) {
+	case core.UITickMsg:
+		p.infoTable.(*InfoTablePane).table.SetRows(p.createRows())
 	case paneResizeMsg:
 		p.height = msg.height
 		p.width = msg.width
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "o":
-			pane := NewCreateColonyPane(
-				"Order Colonization: "+p.planet.Name,
-				p.planet,
-			)
-
-			paneID = PaneManager.AddPane(pane)
-			return p, tea.Sequence(pushDetailStackCmd(paneID), pushFocusStackCmd(paneID))
+			return p.handleColonization()
 		case "s":
-			pane := CreateNewShipManagementPane(
-				"Ship Management",
-				&state.State.ShipManager,
-				func(ship *models.Ship) {
-					order := orders.NewScoutShipOrder(ship, p.planet.Position, state.State.Tick+40)
-					state.State.OrderScheduler.Push(order)
-				},
-			)
-
-			paneID = PaneManager.AddPane(pane)
-			return p, tea.Sequence(pushDetailStackCmd(paneID), pushFocusStackCmd(paneID))
+			return p.handleScoutOrder()
 		case "esc":
-			PaneManager.RemovePane(paneID)
 			return p, tea.Sequence(popDetailStackCmd(), popFocusStackCmd())
 		case "ctrl+c", "q":
 			return p, tea.Quit
 		}
 	}
-	return p, nil
+
+	model, cmd := p.infoTable.Update(msg)
+	p.infoTable = model.(ManagedPane)
+
+	cmds = append(cmds, cmd)
+
+	return p, tea.Batch(cmds...)
 }
 
 func (p *PlanetInfoPane) View() string {
@@ -67,24 +76,9 @@ func (p *PlanetInfoPane) View() string {
 
 	population := fmt.Sprintf("Population: %d", p.planet.Population)
 
-	resources := fmt.Sprintf("\nFood: %10d", p.planet.Food.GetQuantity())
-	resources += fmt.Sprintf("\nMinerals: %10d", p.planet.Minerals.GetQuantity())
-	resources += fmt.Sprintf("\nEnergy:  %10d", p.planet.Energy.GetQuantity())
+	titleStyled := Style.Width(p.width).Align(lipgloss.Center).Bold(true).Render(title)
 
-	constructions := fmt.Sprintf("\nFarms: %10d", len(p.planet.Farms))
-	constructions += fmt.Sprintf("\nMines: %10d", len(p.planet.Mines))
-	constructions += fmt.Sprintf("\nSolar Grids: %10d", len(p.planet.SolarGrids))
-
-	stabilities := fmt.Sprintf("\nHappiness: %10.0f%%", p.planet.Happiness.GetQuantity()*100)
-	stabilities += fmt.Sprintf("\nCorruption: %10.0f%%", p.planet.Corruption.GetQuantity()*100)
-	stabilities += fmt.Sprintf("\nUnrest: %10.0f%%", p.planet.Unrest.GetQuantity()*100)
-
-	title = Style.Width(p.width).Align(lipgloss.Center).Bold(true).Render(title)
-
-	info := lipgloss.JoinVertical(lipgloss.Left, resources, constructions, stabilities)
-	info = Style.Render(info)
-
-	infoContainer := lipgloss.JoinVertical(lipgloss.Left, title, population, info)
+	infoContainer := lipgloss.JoinVertical(lipgloss.Left, titleStyled, population, p.infoTable.View())
 
 	colonizeButton := "Order Colonization: O"
 	scoutButton := "Scout: S"
@@ -100,6 +94,7 @@ func (p *PlanetInfoPane) View() string {
 		lipgloss.Top,
 		infoContainer,
 	)
+
 	content := lipgloss.JoinVertical(lipgloss.Left, topContent, buttons)
 
 	return content
@@ -114,4 +109,67 @@ func NewPlanetInfoPane(title string, planet *models.Planet) *PlanetInfoPane {
 		},
 		planet: planet,
 	}
+}
+
+func (p *PlanetInfoPane) createInfoTable() table.Model {
+
+	columns := []table.Column{
+		{Title: "Stat", Width: 15},
+		{Title: "Quantity", Width: 10},
+		{Title: "Rate", Width: 4},
+	}
+
+	infoTable := table.New(
+		table.WithColumns(columns),
+		table.WithRows(p.createRows()),
+		table.WithFocused(true),
+		table.WithHeight(10),
+	)
+
+	return infoTable
+}
+
+func (p *PlanetInfoPane) createRows() []table.Row {
+
+	rows := []table.Row{
+		{"Food", strconv.Itoa(p.planet.Food.Quantity), strconv.Itoa(p.planet.Food.ConsumptionRate)},
+		{"Minerals", strconv.Itoa(p.planet.Minerals.Quantity), strconv.Itoa(p.planet.Minerals.ConsumptionRate)},
+		{"Energy", strconv.Itoa(p.planet.Energy.Quantity), strconv.Itoa(p.planet.Energy.ConsumptionRate)},
+
+		{"", "", ""},
+		{"Farms", strconv.Itoa(len(p.planet.Farms)), strconv.Itoa(p.planet.GetFarmProduction())},
+		{"Mines", strconv.Itoa(len(p.planet.Mines)), strconv.Itoa(p.planet.GetMineProduction())},
+		{"Solar Grids", strconv.Itoa(len(p.planet.SolarGrids)), strconv.Itoa(p.planet.GetSolarGridProduction())},
+
+		{"", "", ""},
+		{"Happiness", strconv.FormatFloat(float64(p.planet.Happiness.Quantity), 'f', 2, 32), strconv.FormatFloat(float64(p.planet.Happiness.GrowthRate), 'f', 2, 32)},
+		{"Corruption", strconv.FormatFloat(float64(p.planet.Corruption.Quantity), 'f', 2, 32), strconv.FormatFloat(float64(p.planet.Corruption.GrowthRate), 'f', 2, 32)},
+		{"Unrest", strconv.FormatFloat(float64(p.planet.Unrest.Quantity), 'f', 2, 32), strconv.FormatFloat(float64(p.planet.Unrest.GrowthRate), 'f', 2, 32)},
+	}
+
+	return rows
+}
+
+func (p *PlanetInfoPane) handleColonization() (tea.Model, tea.Cmd) {
+	pane := NewCreateColonyPane(
+		"Order Colonization: "+p.planet.Name,
+		p.planet,
+	)
+
+	paneID := PaneManager.AddPane(pane)
+	return p, tea.Sequence(pushDetailStackCmd(paneID), pushFocusStackCmd(paneID))
+}
+
+func (p *PlanetInfoPane) handleScoutOrder() (tea.Model, tea.Cmd) {
+	pane := CreateNewShipManagementPane(
+		"Ship Management",
+		&state.State.ShipManager,
+		func(ship *models.Ship) {
+			order := orders.NewScoutShipOrder(ship, p.planet.Position, state.State.Tick+40)
+			state.State.OrderScheduler.Push(order)
+		},
+	)
+
+	paneID := PaneManager.AddPane(pane)
+	return p, tea.Sequence(pushDetailStackCmd(paneID), pushFocusStackCmd(paneID))
 }
